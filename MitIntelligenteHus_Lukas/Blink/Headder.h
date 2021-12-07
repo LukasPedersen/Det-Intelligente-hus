c2c861dbeaf233d2ea4928f5db2f1672a092d875
Lukas Nørskov Pedersen <luka0591@elevcampus.dk>
2021-12-07 13:15:27 +0100

    FrontDoor

--- MitIntelligenteHus_Lukas/Blink/Headder.h
+++ MitIntelligenteHus_Lukas/Blink/Headder.h
@@ -9,7 +9,18 @@
 #ifndef HEADDER_H_
 #define HEADDER_H_
 
+#include <SPI.h>
+#include <MFRC522.h>
+#include <TimerOne.h>
+#include <Servo.h>
 
+#define SS_PIN 53
+#define RST_PIN 52
+#define RED 12
+#define Green 13
+
+void FrontDoor();
+void DoorLight();
 
 
 
--- MitIntelligenteHus_Lukas/Blink/Main.cpp
+++ MitIntelligenteHus_Lukas/Blink/Main.cpp
@@ -1,12 +1,79 @@
 ﻿#include <Arduino.h>
+
+
 #include "Headder.h"
 
-void setup() 
+unsigned long DELAY_TIME = 2000; // 1 sec
+unsigned long DELAY_LED = 10000;
+unsigned long delayStart = 0; // the time the delay started
+unsigned long delayLed = 0;
+bool delayRunning = false; // true if still waiting for delay to finish
+
+bool switchLED = false;
+
+Servo myservo;
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
+void setup()
 {
+	Serial.begin(9600);		// Initiate a serial communication
+	SPI.begin();			// Initiate  SPI bus
+	mfrc522.PCD_Init();		// Initiate MFRC522
+	Timer1.initialize(5000000);  // Initiate Timer1
+	Timer1.attachInterrupt(DoorLight);
 	
+	//Pin setup
+	myservo.attach(10);
+	pinMode(RED, OUTPUT);
+	pinMode(Green, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
-	
+	FrontDoor();
+	DoorLight();
+}
+
+void DoorLight()
+{
+	if (millis() - delayLed >= DELAY_LED)
+	{
+		
+		delayLed = millis();
+		switchLED = false;
+		myservo.write(90);
+	}
+	digitalWrite(RED, !switchLED);
+	digitalWrite(Green, switchLED);
+}
+
+void FrontDoor()
+{
+	if (millis() - delayStart >= DELAY_TIME)
+	{
+		delayStart = millis();
+		if ( ! mfrc522.PICC_IsNewCardPresent())
+		{
+			
+			return;
+		}
+		if (mfrc522.PICC_ReadCardSerial())
+		{
+			Serial.println("Read card!");
+			String content= "";
+			byte letter;
+			for (byte i = 0; i < mfrc522.uid.size; i++)
+			{
+				content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+				content.concat(String(mfrc522.uid.uidByte[i], HEX));
+			}
+			content.toUpperCase();
+			
+			if (content.substring(1) == "E2 B6 DF 1B")
+			{
+				//Authorized access
+				switchLED = true;
+				myservo.write(0);
+			}
+		}
+	}
 }
